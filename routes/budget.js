130639691b7722ea281020f59ed0ac2677c29425
Priya Reddy <saipriyareddy94@gmail.com>
2020-12-19 01:17:26 -0500

    First commit

---
+++ routes/budget.js
@@ -0,0 +1,48 @@
+const mongoose  = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const budgetModel = require('../model/budget');
+const exjwt = require('express-jwt');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+router.use(cors());
+
+const secretKey = 'My super secret key';
+const jwtMW = exjwt({
+    secret : secretKey,
+    algorithms: ['HS256']
+})
+        
+    router.get('/',jwtMW,(req,res)=>{  
+        console.log(req.params);
+        userid = String(req.query.userid);
+        console.log(userid);
+        budgetModel.find({username:userid})
+    .then((data)=>{
+        res.status(200).send(data);
+    })
+    .catch((err)=>{
+        res.status(500).send();
+    })    
+})
+
+router.post('/',jwtMW, async (req,res)=>{
+    let record = await budgetModel.findOne({$and: [{title:req.body.title},{username:req.body.username}]});
+    if(record) {
+        return res.status(400).send('That expense already exists!');
+    } else {
+    budgetinfo = new budgetModel({
+        title: req.body.title,
+        budget: req.body.budget,
+        maxbudget: req.body.maxbudget,
+        color: req.body.color,
+        username: req.body.username   
+    });
+    
+    await budgetinfo.save();
+    res.send(budgetinfo);
+}
+});
+
+module.exports = router;
