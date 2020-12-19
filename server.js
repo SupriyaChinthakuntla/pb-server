90d7e0bdce8e8507cf45618c7b0891415a053835
Priya Reddy <saipriyareddy94@gmail.com>
2020-12-19 00:37:54 -0500

    server.js changes

--- server.js
+++ server.js
@@ -27,14 +27,14 @@ mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true,useCreate
     .catch(err => console.error('Something went wrong', err));
 
 
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Headers','Content-type,Authorization');
-    next();
-})    
+// app.use((req,res,next)=>{
+//     res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
+//     res.setHeader('Access-Control-Allow-Headers','Content-type,Authorization');
+//     next();
+// })    
 
 app.use(express.json());
-app.use('', express.static('public'));
+// app.use('', express.static('public'));
 app.use('/users', users);    
 app.use('/auth', auth);    
 app.use('/budget', budget);
