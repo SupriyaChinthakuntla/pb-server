4e6e7ece7c2ba665e7dde4b5910483c04249682b
Priya Reddy <saipriyareddy94@gmail.com>
2020-12-19 02:36:48 -0500

    test

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const app = express();
-const config = require('config');
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 const mongoose  = require('mongoose');
-const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
-const budgetModel = require('./model/budget');
 
-
-const userModel = require('./model/users');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 const budget = require('./routes/budget');
-
-
-const accessTokenKey = 'My super secret key';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -28,19 +20,12 @@ mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true,useCreate
     .then(() => console.log('Now connected to MongoDB!'))
     .catch(err => console.error('Something went wrong', err));
 
-
-// app.use((req,res,next)=>{
-//     res.setHeader('Access-Control-Allow-Origin','http://68.183.139.30:3000');
-//     res.setHeader('Access-Control-Allow-Headers','Content-type,Authorization');
-//     next();
-// })    
+ 
 
 app.use(express.json());
 app.use('/users', users);    
 app.use('/auth', auth);    
 app.use('/budget', budget);
-//app.use('',express.static('public'));
-
 
 
 
