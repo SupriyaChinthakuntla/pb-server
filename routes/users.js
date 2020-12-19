505be37df0017390b6f46ff6edc312f9ac2f5416
Priya Reddy <saipriyareddy94@gmail.com>
2020-12-19 03:53:57 -0500

    CORS

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ const userModel = require('../model/users');
 const express = require('express');
 const router = express.Router();
  
+const cors = require('cors');
+router.use(cors());
+
 router.post('/', async (req, res) => {       
  
     // Check if this user already exisits
