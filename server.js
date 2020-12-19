8383282ed6eb2a452351cbb54e39702f45fffd8e
Priya Reddy <saipriyareddy94@gmail.com>
2020-12-19 12:21:07 -0500

    test

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const port = process.env.PORT || 3000;
 const mongoose  = require('mongoose');
 const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 const budget = require('./routes/budget');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
 
 var url = 'mongodb+srv://supriya:12345@cluster0.pa8f0.mongodb.net/personalBudget?retryWrites=true&w=majority';
 
@@ -28,10 +30,10 @@ mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true,useCreate
     })  
 
 app.use(express.json());
+app.use('', express.static('public'));
 app.use('/users', users);    
 app.use('/auth', auth);    
 app.use('/budget', budget);
-
 
 
 app.listen(port,()=>{
